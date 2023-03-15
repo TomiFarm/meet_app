4824d2e6c484f3c89573bcd69471a279c8577301
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2023-03-15 15:01:01 +0100

    fixing issue

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
-import './nprogress.css'
+import './nprogress.css';
 
 
 class App extends Component {
