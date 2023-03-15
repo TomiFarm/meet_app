13f80f974c3f134f8d6957b96bb30da8083a8ee6
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2023-03-15 16:06:55 +0100

    fixing issue

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
     const { access_token } = await fetch(
-        'https://0v0traqf9e.execute-api.eu-central-1.amazonaws.com/dev/api/token' + encodeCode
+        'https://0v0traqf9e.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
     )
     .then((res) => {
         return res.json();
