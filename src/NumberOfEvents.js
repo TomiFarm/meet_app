09495f395989dc6e2d0fd42c284573b532a77927
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2023-03-23 16:27:07 +0100

    Added error alert for false number to show

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
     state = {
-        query: 32
+        query: 32,
+        errorText: ''
     };
 
     handleInputChanged = (numberOfEvents) => {
         const value = numberOfEvents.target.value;
-        this.setState({query: value});
-        this.props.updateEvents(null, value);
+        if(value > 0 && value < 100) {
+            this.setState({query: value, errorText: ''});
+            this.props.updateEvents(null, value);
+        } else {
+            return this.setState({errorText: 'Please select a number between 1 and 99'});
+        }
     };
 
     render() {
         
         return (
             <div className="numberOfEvents">
+                <ErrorAlert text={this.state.errorText} />
                 <label className="numberLabel">Number of events:</label>
                 <input
                     type="number"
