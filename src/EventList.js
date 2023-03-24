552803f3e1f577ab2b67f751280391a0fdebec78
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2023-03-24 15:41:18 +0100

    Added warning for offline

--- src/EventList.js
+++ src/EventList.js
@@ -12,23 +12,23 @@ class EventList extends Component {
 
         if(!navigator.onLine){
             this.setState({
-              offlineWarningText: 'You are offline. The events shown may not be up to date. Please connect to internet to make searches and to get up to date events.'
-          });
-          } else {
-            this.setState({
-              offlineWarningText: ''
+                offlineWarningText: 'You are offline. The events shown may not be up to date. Please connect to internet to make searches and to get up to date events.'
             });
-          }
+        } else {
+            this.setState({
+                offlineWarningText: ''
+            });
+        }
 
         return (
+            <><WarningAlert text={this.state.offlineWarningText} />
             <ul className="EventList">
-                <WarningAlert text={this.state.offlineWarningText} />
                 {events.map(event => 
                     <li key={event.id}>
                         <Event event={event} />
                     </li>
                 )}
-            </ul>
+            </ul></>
         );
     }
 }
