5136eb4e7ee9488531accb223db57f78d6d44acd
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2023-03-25 17:39:50 +0100

    added chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import './App.css';
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 import EventList from './EventList';
 import CitySearch from './CitySearch';
@@ -40,6 +42,16 @@ class App extends Component {
 
   componentWillUnmount(){
     this.mounted = false;
+  };
+
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length;
+      const city = location.split(', ').shift();
+      return { city, number };
+    });
+    return data;
   };
 
   updateEvents = (location, selectedEventCount) => {
@@ -87,10 +99,26 @@ class App extends Component {
     return (
       <div className="App">
         <WarningAlert text={warningText} />
+        <h1>Meet App</h1>
+        <h4>Choose a city</h4>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents eventCount={this.state.eventCount} updateEvents={this.updateEvents} />
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400} >
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={ () => {getAccessToken()} } />
+        {/* <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={ () => {getAccessToken()} } /> */}
       </div>
     );
   };
